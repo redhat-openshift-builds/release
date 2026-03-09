avinal/builds-release

ci: add github action to periodally update the task bundle
- update task bundle on every Monday
- fix quality issue with the script

Signed-off-by: Avinal Kumar <avinal@redhat.com>

scripts/update-task-bundles.sh
@@ -1,61 +1,88 @@
 #!/bin/bash
 
 # Use this script to update the Tekton Task Bundle references to the LATEST clean version tag.
-# update-pipelines.sh .tekton/*.yaml
+# Usage: update-task-bundles.sh pipelines/*.yaml
 
 set -euo pipefail
 
-FILES=$*
+if [[ $# -eq 0 ]]; then
+    echo "Usage: $0 <file1.yaml> [file2.yaml ...]"
+    exit 1
+fi
+
+FILES=("$@")
 
 # Find existing image references
-OLD_REFS="$( \
+mapfile -t OLD_REFS < <(
   yq -N \
   '... | select(has("resolver")) |
   .params // [] | .[] |
   select(.name == "bundle") |
   .value' \
-  $FILES | \
-  sort -u \
-)"
+  "${FILES[@]}" | \
+  sort -u
+)
+
+if [[ ${#OLD_REFS[@]} -eq 0 ]]; then
+    echo "No bundle references found in the provided files."
+    exit 0
+fi
+
+# Cache: repo -> "latest_tag digest" to avoid duplicate registry lookups
+declare -A REPO_CACHE
 
 # Find updates for image references
-for old_ref in ${OLD_REFS}; do
+for old_ref in "${OLD_REFS[@]}"; do
     # 1. Extract the repository name (e.g., quay.io/my-org/my-bundle)
     repo_tag="${old_ref%@*}"
     repo="${repo_tag%:*}"
 
-    echo -e "\nChecking for new versions of ${repo}..."
+    echo ""
+    echo "Checking for new versions of ${repo}..."
 
-    # 2. Get all tags that look like versions
-    version_tags=$(skopeo list-tags "docker://${repo}" | \
-                   yq '.Tags[]' | \
-                   grep -E '^[0-9]+\.[0-9]+' || true)
+    # 2. Check cache for this repo, otherwise query the registry
+    if [[ -n "${REPO_CACHE[$repo]+x}" ]]; then
+        latest_tag="${REPO_CACHE[$repo]%% *}"
+        new_digest="${REPO_CACHE[$repo]#* }"
+    else
+        # Get all tags that look like versions
+        version_tags=$(skopeo list-tags "docker://${repo}" | \
+                       yq '.Tags[]' | \
+                       grep -E '^[0-9]+\.[0-9]+' || true)
 
-    if [[ -z "$version_tags" ]]; then
-        echo -e "\n--> Could not find any valid version tags for ${repo}. Skipping."
-        continue
+        if [[ -z "$version_tags" ]]; then
+            echo "--> Could not find any valid version tags for ${repo}. Skipping."
+            continue
+        fi
+
+        # 3. Find the highest version number by stripping suffixes before sorting
+        latest_version_num=$(echo "$version_tags" | sed 's/-.*//' | sort -V | tail -n1)
+
+        # 4. From all tags matching that version, prefer the clean one (e.g., "0.1" over "0.1-hash")
+        candidate_tags=$(echo "$version_tags" | grep "^${latest_version_num}")
+        latest_tag=$(echo "$candidate_tags" | grep -E "^${latest_version_num}$" || echo "$candidate_tags" | head -n1)
+
+        # 5. Resolve the digest for the chosen tag
+        new_digest="$(skopeo inspect --no-tags "docker://${repo}:${latest_tag}" | yq '.Digest')" || {
+            echo "--> Failed to inspect ${repo}:${latest_tag}. Skipping."
+            continue
+        }
+
+        # Cache the result
+        REPO_CACHE[$repo]="${latest_tag} ${new_digest}"
     fi
 
-    # 3. Find the highest version number by stripping suffixes before sorting
-    latest_version_num=$(echo -e "\n${version_tags}" | sed 's/-.*//' | sort -V | tail -n1)
+    # 6. Construct the new reference
+    new_ref="${repo}:${latest_tag}@${new_digest}"
 
-    # 4. From all tags matching that version, prefer the clean one (e.g., "0.1" over "0.1-hash")
-    candidate_tags=$(echo -e "\n${version_tags}" | grep "^${latest_version_num}")
-    latest_tag=$(echo -e "\n${candidate_tags}" | grep -E "^${latest_version_num}$" || echo -e "\n${candidate_tags}" | head -n1)
-
-    # 5. Construct the new reference with the chosen latest tag
-    new_repo_tag="${repo}:${latest_tag}"
-    new_digest="$(skopeo inspect --no-tags "docker://${new_repo_tag}" | yq '.Digest')"
-    new_ref="${new_repo_tag}@${new_digest}"
-
-    # 6. If the new reference is different, update the files
+    # 7. If the new reference is different, update the files
     if [[ "$new_ref" == "$old_ref" ]]; then
-        echo -e "\n--> ${old_ref} is already up-to-date."
+        echo "--> ${old_ref} is already up-to-date."
         continue
     fi
 
-    echo -e "\nNew version found! Updating to ${new_ref}"
-    for file in $FILES; do
+    echo "New version found! Updating to ${new_ref}"
+    for file in "${FILES[@]}"; do
         sed -i "s#${old_ref}#${new_ref}#g" "$file"
     done
 done

redhat-openshift-builds/release

scripts/update-task-bundles.sh
@@ -1,61 +1,90 @@
 #!/bin/bash
 
 # Use this script to update the Tekton Task Bundle references to the LATEST clean version tag.
-# update-pipelines.sh .tekton/*.yaml
+# Usage: update-task-bundles.sh pipelines/*.yaml
 
 set -euo pipefail
 
-FILES=$*
+if [[ $# -eq 0 ]]; then
+    echo "Usage: $0 <file1.yaml> [file2.yaml ...]"
+    exit 1
+fi
+
+FILES=("$@")
 
 # Find existing image references
-OLD_REFS="$( \
+mapfile -t OLD_REFS < <(
   yq -N \
   '... | select(has("resolver")) |
   .params // [] | .[] |
   select(.name == "bundle") |
   .value' \
-  $FILES | \
-  sort -u \
-)"
+  "${FILES[@]}" | \
+  sort -u
+)
+
+if [[ ${#OLD_REFS[@]} -eq 0 ]]; then
+    echo "No bundle references found in the provided files."
+    exit 0
+fi
+
+# Cache: repo -> "latest_tag digest" to avoid duplicate registry lookups
+declare -A REPO_CACHE
 
 # Find updates for image references
-for old_ref in ${OLD_REFS}; do
+for old_ref in "${OLD_REFS[@]}"; do
     # 1. Extract the repository name (e.g., quay.io/my-org/my-bundle)
     repo_tag="${old_ref%@*}"
     repo="${repo_tag%:*}"
 
-    echo -e "\nChecking for new versions of ${repo}..."
+    echo ""
+    echo "Checking for new versions of ${repo}..."
 
-    # 2. Get all tags that look like versions
-    version_tags=$(skopeo list-tags "docker://${repo}" | \
-                   yq '.Tags[]' | \
-                   grep -E '^[0-9]+\.[0-9]+' || true)
+    # 2. Check cache for this repo, otherwise query the registry
+    if [[ -n "${REPO_CACHE[$repo]+x}" ]]; then
+        latest_tag="${REPO_CACHE[$repo]%% *}"
+        new_digest="${REPO_CACHE[$repo]#* }"
+    else
+        # Get all tags that look like versions
+        version_tags=$(skopeo list-tags "docker://${repo}" | \
+                       yq '.Tags[]' | \
+                       grep -E '^[0-9]+\.[0-9]+' || true)
 
-    if [[ -z "$version_tags" ]]; then
-        echo -e "\n--> Could not find any valid version tags for ${repo}. Skipping."
-        continue
+        if [[ -z "$version_tags" ]]; then
+            echo "--> Could not find any valid version tags for ${repo}. Skipping."
+            continue
+        fi
+
+        # 3. Find the highest version number by stripping suffixes before sorting
+        latest_version_num=$(echo "$version_tags" | sed 's/-.*//' | sort -V | tail -n1)
+
+        # 4. From all tags matching that version, prefer the clean one (e.g., "0.1" over "0.1-hash")
+        escaped_version=$(printf '%s' "$latest_version_num" | sed 's/[.[\*^$()+?{|\\]/\\&/g')
+        candidate_tags=$(echo "$version_tags" | grep "^${escaped_version}")
+        latest_tag=$(echo "$candidate_tags" | grep -E "^${escaped_version}$" || echo "$candidate_tags" | head -n1)
+
+        # 5. Resolve the digest for the chosen tag
+        new_digest="$(skopeo inspect --no-tags "docker://${repo}:${latest_tag}" | yq '.Digest')" || {
+            echo "--> Failed to inspect ${repo}:${latest_tag}. Skipping."
+            continue
+        }
+
+        # Cache the result
+        REPO_CACHE[$repo]="${latest_tag} ${new_digest}"
     fi
 
-    # 3. Find the highest version number by stripping suffixes before sorting
-    latest_version_num=$(echo -e "\n${version_tags}" | sed 's/-.*//' | sort -V | tail -n1)
+    # 6. Construct the new reference
+    new_ref="${repo}:${latest_tag}@${new_digest}"
 
-    # 4. From all tags matching that version, prefer the clean one (e.g., "0.1" over "0.1-hash")
-    candidate_tags=$(echo -e "\n${version_tags}" | grep "^${latest_version_num}")
-    latest_tag=$(echo -e "\n${candidate_tags}" | grep -E "^${latest_version_num}$" || echo -e "\n${candidate_tags}" | head -n1)
-
-    # 5. Construct the new reference with the chosen latest tag
-    new_repo_tag="${repo}:${latest_tag}"
-    new_digest="$(skopeo inspect --no-tags "docker://${new_repo_tag}" | yq '.Digest')"
-    new_ref="${new_repo_tag}@${new_digest}"
-
-    # 6. If the new reference is different, update the files
+    # 7. If the new reference is different, update the files
     if [[ "$new_ref" == "$old_ref" ]]; then
-        echo -e "\n--> ${old_ref} is already up-to-date."
+        echo "--> ${old_ref} is already up-to-date."
         continue
     fi
 
-    echo -e "\nNew version found! Updating to ${new_ref}"
-    for file in $FILES; do
-        sed -i "s#${old_ref}#${new_ref}#g" "$file"
+    echo "New version found! Updating to ${new_ref}"
+    escaped_old_ref=$(printf '%s' "$old_ref" | sed 's/[.[\*^$()+?{|\\/#]/\\&/g')
+    for file in "${FILES[@]}"; do
+        sed -i "s#${escaped_old_ref}#${new_ref}#g" "$file"
     done
 done